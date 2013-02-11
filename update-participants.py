d6147518cdc48012801ae22c17d4e0c28c5edad2
Marcus -Chaosblog <chaosblog@debilbox.de>
2013-02-11 13:07:56 +0100

    Add current week for "skipped" tag calculation

--- update-participants.py
+++ update-participants.py
@@ -6,6 +6,7 @@ import sys
 import xmlrpclib
 import subprocess
 import settings
+from datetime import datetime
 
 config=settings.load_settings()
 
@@ -19,7 +20,7 @@ finally:
 x = xmlrpclib.ServerProxy(config['xmlrpc_endpoint'])
 page = x.wp.getPage(config['blog_id'], config['participants_page_id'], config['username'], passwd)
 
-text = render.render_template('templates/users.tmpl')
+text = render.render_template('templates/users.tmpl',datetime.now().strftime("%Y/%m/%d"))
 page['description'] = text
 
 x.wp.editPage(config['blog_id'], config['participants_page_id'], config['username'], passwd,page,True)
